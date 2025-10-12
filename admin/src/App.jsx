1bee2f415e51386b44c30f4f295faf7a98711cc2
tapash_since-2006 <upadhyayt15@gmail.com>
2025-10-12 20:43:22 +0545

    v2.1

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
         <Routes>
 
           {/* Admin Route */}
-          <Route path='/' element={<></>}/>
+          <Route path='/' element={<Dashboard/>}/>
           <Route path='/admin-dashboard' element={<Dashboard/>}/>
           <Route path='/all-appointments' element={<AllAppointments/>}/>
           <Route path='/add-doctor' element={<AddDoctor/>}/>

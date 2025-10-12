bceb300b95b63d2aa9268872055fa0381a8c3e88
tapash-since-2006 <upadhyayt15@gmail.com>
2025-10-12 23:59:19 +0545

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -11,7 +11,7 @@ const AdminContextProvider=(props)=>{
     const [appointments, setAppointments]=useState([])
     const[dashData, setDashData]=useState(null)
 
-    const backendUrl=import.meta.env.VITE_BACKEND_URL
+    const backendUrl="https://appointmentbooking-08it.onrender.com"
 
     const getAllDoctors=async()=>{
         try {
